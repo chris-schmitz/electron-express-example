b4fd84e6fc57597c5b7082eac116628885c4e163
chris-schmitz <schmitz.chris@gmail.com>
2017-09-13 20:44:27 -0500

    init of concept

---
+++ src/renderer/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button @click="startServer">Start</button>
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage'
+  import {ipcRenderer} from 'electron'
+
+  export default {
+    name: 'electron-express',
+    components: {
+      LandingPage
+    },
+    methods: {
+      startServer () {
+        ipcRenderer.send('start')
+      }
+    },
+    created () {
+      ipcRenderer.on('started', payload => {
+        console.log(payload)
+      })
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
